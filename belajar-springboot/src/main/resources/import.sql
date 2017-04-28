fc9872e256ae81e78377a5f7aceb60a9fd13a127
adhasyuhada <adha.teknik@gmail.com>
2017-04-28 22:56:26 +0700

    ignore .*

--- belajar-springboot/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into m_product (code,name,price) values ('P-001','Product 001',15000.00);
-insert into m_product (code,name,price) values ('P-002','Product 002',25000.00);
-insert into m_product (code,name,price) values ('P-003','Product 003',35000.00);
-insert into m_product (code,name,price) values ('P-004','Product 004',45000.00);
