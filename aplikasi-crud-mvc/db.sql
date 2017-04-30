a5a13ede19287e9258b75fba38271d1f53965311
adhasyuhada <adha.teknik@gmail.com>
2017-04-30 17:30:09 +0700

    update db.sql

--- aplikasi-crud-mvc/db.sql
+++ aplikasi-crud-mvc/db.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-CREATE database demo;
-CREATE TABLE peranan(id int(11) AUTO_INCREMENT, username varchar(25), peran varchar(250), description varchar(250),PRIMARY KEY (id));
-=======
 create database demo;
 CREATE TABLE peranan(id int(11) AUTO_INCREMENT, username varchar(25), peran varchar(250), description varchar(250),PRIMARY KEY (id));
->>>>>>> 09eeb692c3650ff3f8ec406856645346eee95d80
